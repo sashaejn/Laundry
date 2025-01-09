81f002460e785c4b5de68e1b58f0a5aa6a666cf8
Aisyah <aisyahsawitrii@gmail.com>
2025-01-09 10:39:24 +0700

    design portrait selesai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.aisyah.laundry"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.aisyah.laundry"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
