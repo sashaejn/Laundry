1565ff25246c51cc7076e15422c75c01240ac5b5
aisyashwty <148684638+sashaejn@users.noreply.github.com>
2025-01-24 13:24:41 +0700

    penambahan activity baru (data pelanggan dan tambah pelanggan)

---
+++ app/src/main/java/com/aisyah/laundry/pelanggan/TambahPelanggan.kt
@@ -0,0 +1,21 @@
+package com.aisyah.laundry.pelanggan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aisyah.laundry.R
+
+class TambahPelanggan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tambah_pelanggan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
